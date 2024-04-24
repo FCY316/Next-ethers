ea0982de1cb8e5fffec88d6b73a15d98156a58b4
冯承运 <790322000@qq.com>
2024-04-24 15:32:44 +0800

    next+ethers 第一次提交

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // 打包模式
+  reactStrictMode: false, // 严格模式
+  env: {}, // 设置全局参数
+};
 
 export default nextConfig;
